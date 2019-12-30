3934f8fbc6dce959a36367959f2437feabcd24df
CatMan <garfield_lv@163.com>
2019-12-30 15:05:54 +0800

    backend

--- case01/tasks.py
+++ case01/tasks.py
@@ -1,6 +1,9 @@
 from celery import Celery
 
-app = Celery("tasks",broker="redis://localhost")
+#app = Celery("tasks",broker="redis://localhost")
+#app = Celery("tasks",broker="pyamqp://",backend="redis://localhost")
+app = Celery("tasks",broker="redis://localhost:6379/1",backend="redis://localhost:6379/2")
+
 
 @app.task
 def add(x,y):
